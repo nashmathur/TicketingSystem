2d456df25abe1622f9de4692b370d87fa2ffece4
Natansh Mathur <nashmathur@gmail.com>
2018-06-27 01:02:24 +0530

    Added Login Form Frontend

--- TicketingApp/ticketing/urls.py
+++ TicketingApp/ticketing/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'rest-auth/', include('rest_auth.urls')),
     url(r'rest-auth/registration/', include('rest_auth.registration.urls')),
-
 ]
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,79 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button, Form, } from 'semantic-ui-react'
+
+
+class LoginForm extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      csrf_token: '',
+    };
+  }
+
+  componentDidMount(){
+    const csrftoken = this.getCookie('csrftoken');
+    this.setState({csrf_token: csrftoken});
+  }
+
+  getCookie(name) {
+    if (!document.cookie) {
+      return null;
+    }
+
+    const xsrfCookies = document.cookie.split(';')
+      .map(c => c.trim())
+      .filter(c => c.startsWith(name + '='));
+
+    if (xsrfCookies.length === 0) {
+      return null;
+    }
+
+    return decodeURIComponent(xsrfCookies[0].split('=')[1]);
+  }
+
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }    
+
+  //     let user = JSON.parse(localStorage.getItem('user'));
+
+  render = () => {
+    return (
+      <Form onSubmit={(e) => this.handleSubmit(e)} >
+        Login: 
+        <Form.Field>
+          <label>Username</label>
+          <input name='username' onChange = {(e) => this.handleChange(e)} value={this.state.username} placeholder='Username' />
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <input name='password' onChange={(e) => this.handleChange(e)} value={this.state.password} type='password' placeholder='Password' />
+        </Form.Field>
+        <input type='submit'/>
+      </Form>
+    )     
+  }
+}
+
 
 class App extends Component {
   state = {
-    tickets : []
+    tickets : [],
+    newticket : {
+      title : '',
+      description: '',
+      category : '',
+    }
   };
 
   async componentDidMount() {
@@ -22,12 +92,7 @@ class App extends Component {
     return (
       <div>
         <div>
-          Login:
-          <form action="127.0.0.1:8000/api-auth/login" method="POST">
-            Username: <input type="text" name="username"/>
-            Password: <input type="password" name="password"/>
-            <input type="submit"/>
-          </form>
+          <LoginForm />
         </div>
         <div>
           <h1> Tickets </h1> <br/>
